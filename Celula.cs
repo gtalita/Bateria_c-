f4f267e4d46c23fa8ced0df6e7130c568b2f451e
gtalita <167032543+gtalita@users.noreply.github.com>
2024-06-29 20:03:37 -0300

    Add files via upload

---
+++ Celula.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tstebateria
+{
+    internal class Celula
+    {
+        private float carga = 25f;
+        private static float cargaBateria;
+
+        
+
+        public float GetCarga()
+        {
+            return carga;
+        }
+
+        public float GetCargaBateria()
+        {
+            cargaBateria = carga * 4;
+            return cargaBateria;
+        }
+
+        public void Consumir()
+        {
+            carga -= 5f;
+           
+        }
+
+
+        public void Carregar()
+        {
+            carga += 5f;
+            
+        }
+
+    }
+}
